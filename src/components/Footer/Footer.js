867ab2967f99a072bafb9b1741601986fa6efce0
Luke <deandrevidal@aol.com>
2021-11-18 17:00:24 -0800

    fixed footer and slashes in img tags

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="Footer">
-            <h1>Feet</h1>
+  return (
+    <div className="Footer">
+      <div className="wrapper">
+        <div className="Footer-Section">
+          <h2>Contact Us</h2>
+          <ul>
+            <li>Email: support@sfpopos.com</li>
+            <li>Phone: (555)-123-456</li>
+            <li>Address: All over the world.</li>
+          </ul>
         </div>
-    )
+        <div className="Footer-Section">
+          <h2>Other Services:</h2>
+          <ul>
+            <li>Exisiting</li>
+            <li>Nature Sounds</li>
+            <li>Natural Lighting Specialists</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
